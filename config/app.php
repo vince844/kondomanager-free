18585270358262026f711ed3d52d6eb7f522070d
vincenzo vecchio <vince.karibusana@gmail.com>
2025-04-14 18:12:57 +0100

    Inzio lavori creazione bacheca condominio
    1. Inzio versione 1.2.0
    
    Signed-off-by: vincenzo vecchio <vince.karibusana@gmail.com>

--- config/app.php
+++ config/app.php
@@ -25,7 +25,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => env('APP_VERSION', '1.1.0'),
+    'version' => env('APP_VERSION', '1.2.0'),
 
     /*
     |--------------------------------------------------------------------------
