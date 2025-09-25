854813653754abc35d2b44d5993e2a2215312fe3
vincenzo vecchio <vince.karibusana@gmail.com>
2025-09-25 06:06:52 +0100

    Inizio versione 1.7.0
    Inizio lavori versione 1.7.0
    
    Signed-off-by: vincenzo vecchio <vince.karibusana@gmail.com>

--- config/app.php
+++ config/app.php
@@ -25,7 +25,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => env('APP_VERSION', '1.6.0'),
+    'version' => env('APP_VERSION', '1.7.0'),
 
     /*
     |--------------------------------------------------------------------------
