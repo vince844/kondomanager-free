707c7568766887fa814302674b40713b68d78de6
vincenzo vecchio <vince844@users.noreply.github.com>
2025-10-03 07:24:44 +0100

    Update welcome.blade.php

--- resources/views/vendor/installer/livewire/install/welcome.blade.php
+++ resources/views/vendor/installer/livewire/install/welcome.blade.php
@@ -1,38 +1,38 @@
 <div class="h-full flex flex-col">
-    <p class="mb-6">This wizard will guide you through the setup process for <strong>{{ $appName }}</strong>.</p>
+    <p class="mb-6">Questa procedura guidata ti guiderà attraverso il processo di installazione e configurazione di <strong>{{ $appName }}</strong>.</p>
 
-    <p>Before we start, please make sure you have the following information and requirements are with you.</p>
+    <p>Prima di iniziare, per favore assicurati di avere le seguenti informazioni e requisiti minimi del server.</p>
 
     <ul class="ps-6 list-decimal mt-6 space-y-6">
         <li>
-            <strong>PHP:</strong> Your server must have **PHP version {{ $php }} or higher.
-            <a class="link block" href="#">How to check my PHP version</a>
+            <strong>PHP:</strong> Il server deve avere installato la versione {{ $php }} o maggiore.
+            <a class="link block" href="https://www.php.net/manual/en/function.phpversion.php">Come controllare la versione di PHP installata</a>
         </li>
 
         @isset($extensions)
-            <li><strong>Extensions:</strong> Please be sure the following extensions are enabled in you php configuration.
+            <li><strong>Extensioni:</strong> Assicurati di aver le seguenti estensioni abilitate nella configurazione di PHP.
                 <p>{{ $extensions }}</p>
-                <a class="link block" href="#">How to enable a PHP extension</a>
+                <a class="link block" target="_blank" href="https://www.php.net/manual/en/function.extension-loaded.php">Come controllare quali estensioni PHP installate</a>
             </li>
         @endisset
 
         @if ($isDatabaseRequired)
             <li>
-                <strong>Database:</strong> The application needs a database to record data. Please have your **database name, username, and password** ready with you.
-                <a class="link block" href="#">Learn how to create a database</a>
+                <strong>Database:</strong> L'applicazione necessita di un database MySQL per la registrazione dei dati. Per favore assicurati di aver creato un database e di avere i seguenti dati: host, porta, nome del database, username e password.
+                <a class="link block" target="_blank" href="https://support.cpanel.net/hc/en-us/articles/360057550753-How-to-create-a-database-and-database-user-in-cPanel">Come creare un database su cPanel</a>
             </li>
         @endif
 
 
         @if ($isMailRequired)
             <li>
-                <strong>Mail Settings:</strong> The application sends important emails, such as password resets and notifications. You will need a valid **email service** to send these. Please ensure you have a valid email id and password ready aling with server host and port configuration.
-                <a class="link block" href="#">How to know email server configurations</a>
+                <strong>Impostazioni email:</strong> L'applicazione invia e-mail importanti come: registrazione utenti, impostazione password e notifiche. Per inviarle è necessario un servizio di posta elettronica valido. Assicurati di avere a portata di mano un indirizzo email e una password validi, in linea con la configurazione dell'host e della porta del server.
+                <a class="link block" target="_blank"href="https://www.smtper.net/">Testa la configurazione SMTP</a>
             </li>
         @endif
 
         <li>
-            <strong>Clear Cache:</strong> Clear any cache memory in server.
+            <strong>Pulisci la cache:</strong> Se necessario pulisci la cache del server.
         </li>
     </ul>
 </div>
