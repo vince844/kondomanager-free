ea3ce18828c4affe08f81d5bf6ad6129454a222e
k3ntinhu <k3ntinhu@gmail.com>
2026-01-12 00:23:44 +0000

    Revert "add dashboard search and button translations"
    This reverts commit a00a9aef71a771ea15b25c0d8c38b5544faba5aa.

--- lang/pt/dashboard.php
+++ lang/pt/dashboard.php
@@ -2,23 +2,4 @@
 
 return [
     'condomini_registrati'    => "Edíficios registados",
-
-    /* ------------------------------------------------------------------
-     | search
-     | ------------------------------------------------------------------ */
-    'search' => [
-        'cerca_condominio'          => "Procurar condomínio... ",
-        'nessun_condominio_trovato' => "Nenhum condomínio encontrado.",
-        'caricamento'               => "A carregar...",
-    ],
-
-    /* ------------------------------------------------------------------
-     | button
-     | ------------------------------------------------------------------ */
-    'buttons' => [
-        'vai_al_gestionale_condominio' => "Ir para a gestão do condomínio",
-        'crea_condominio'              => "Criar condomínio",
-        'reset_selezione'              => "Repor seleção",
-    ],
-
-]; 
+];
