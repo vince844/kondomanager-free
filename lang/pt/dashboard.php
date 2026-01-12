d9e36ca53314c67fe022402f8d74c92e7b3526bd
k3ntinhu <k3ntinhu@gmail.com>
2026-01-12 00:29:49 +0000

    minor adjustment for dashboard

--- lang/pt/dashboard.php
+++ lang/pt/dashboard.php
@@ -2,4 +2,23 @@
 
 return [
     'condomini_registrati'    => "Edíficios registados",
+
+    /* ------------------------------------------------------------------
+     | search
+     | ------------------------------------------------------------------ */
+    'search' => [
+        'cerca_condominio'          => "Procurar condomínio... ",
+        'nessun_condominio_trovato' => "Nenhum condomínio encontrado.",
+        'caricamento'               => "A carregar...",
+    ],
+
+    /* ------------------------------------------------------------------
+     | button
+     | ------------------------------------------------------------------ */
+    'buttons' => [
+        'vai_al_gestionale_condominio' => "Gestão",
+        'crea_condominio'              => "Criar condomínio",
+        'reset_selezione'              => "Repor seleção",
+    ],
+
 ];
